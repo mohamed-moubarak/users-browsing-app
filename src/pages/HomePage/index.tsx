7740453da84c7c90837ebec4a98ca7eed9b19448
Mohamed Moubarak <mohamed.hisham@zyda.com>
2021-11-16 00:43:18 +0200

    Update HomePage shimmerEffect

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -9,8 +9,7 @@ import UserCardShimmer from "../../components/UserCard/shimmer";
 
 const HomePage: React.FC = () => {
  const [users, setUsers] = useState<Array<UserModel>>();
-
- const { data, isLoading } = useUsersList({ page: 1, limit: 10 });
+ const { data, isLoading } = useUsersList({ page: 1, limit: 24 });
 
  useEffect(() => {
   setUsers(data?.data);
@@ -18,26 +17,28 @@ const HomePage: React.FC = () => {
 
  return (
   <StyledPageWrapper>
-    <Typography variant="h2" mt={2} mx="auto">
-     Users
-    </Typography>
+   <Typography variant="h2" mx="auto">
+    Users
+   </Typography>
 
    <Grid container spacing={3} p={2}>
-    {users?.map((user) => {
+    {isLoading && Array.from(Array(24).keys()).map((_, i) => (
+     <Grid item key={i} xs={12} md={6} lg={4}>
+      <UserCardShimmer />
+     </Grid>
+    ))}
+
+    {!isLoading && users?.map((user) => {
      const { id, picture, title, firstName, lastName } = user;
      return (
       <Grid item key={id} xs={12} md={6} lg={4}>
-       {isLoading ? (
-        <UserCardShimmer />
-       ) : (
-        <UserCard
-         id={id}
-         picture={picture}
-         title={title}
-         firstName={firstName}
-         lastName={lastName}
-        />
-       )}
+       <UserCard
+        id={id}
+        picture={picture}
+        title={title}
+        firstName={firstName}
+        lastName={lastName}
+       />
       </Grid>
      );
     })}
